be844456f256d85a443e4e02bade234ea6c293e2
Mayur-Murarka <137669111+Mayur-Murarka@users.noreply.github.com>
2024-08-25 17:22:31 +0530

    features.js modified

--- utils/features.js
+++ utils/features.js
@@ -12,7 +12,7 @@ const cookieOptions = {
 };
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
+    const uri = process.env.MONGO_URI;
     if (!uri) {
       throw new Error('MongoDB URI is not defined in the environment variables.');
     }
